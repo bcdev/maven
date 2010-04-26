fbdd2a0ef278188a89cc737d91f1a4ffaa2fae68
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-04-26 13:28:15 +0200

    update to 4.8-SNAPSHOT

--- maven-beam-gpf-archetype/gpopgen.bat
+++ maven-beam-gpf-archetype/gpopgen.bat
@@ -4,7 +4,7 @@ rem
 mvn archetype:generate  ^
  -DarchetypeGroupId=org.esa.beam.maven ^
  -DarchetypeArtifactId=maven-beam-gpf-archetype ^
- -DarchetypeVersion=4.7.1 ^
+ -DarchetypeVersion=4.8-SNAPSHOT ^
  -DarchetypeRepository=http://www.brockmann-consult.de/mvn/os ^
  -DgroupId=%1 ^
  -DartifactId=%2 ^
